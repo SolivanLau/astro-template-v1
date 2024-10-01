338a07430fc6864746fbadad847210e851a2591f
solivan-lau <solivan_lau@outlook.com>
2024-10-01 14:31:24 -0400

    DEPLOYMENT: netlify toml for context deploys. added different build commands

--- src/assets/scripts/animations.ts
+++ src/assets/scripts/animations.ts
@@ -2,4 +2,3 @@
 // // query for all elements with data-animate-scroll attribute
 // // add animation class to them when element is scrolled into viewport
 // // scss: consider different values for data-animate-scroll attribute
-
